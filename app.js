5c66c96f75e5474ab165dacfa270ee33dcced5f2
HyunjiLee <hyunji.lee.tech@gmail.com>
2022-04-21 21:23:08 -0400

    Added page change animation

--- app.js
+++ app.js
@@ -5,16 +5,44 @@ const allSections = document.querySelector(".main-content");
 
 function PageTransitions() {
   //Button click active class
-  for (let i = 0; i < sectBtn.length; i++) {
-    sectBtn[i].addEventListener("click", function () {
+  //   for (let i = 0; i < sectBtn.length; i++) {
+  //     sectBtn[i].addEventListener("click", function () {
+  //       let currentBtn = document.querySelectorAll(".active-btn");
+  //       currentBtn[0].className = currentBtn[0].className.replace(
+  //         "active-btn",
+  //         ""
+  //       );
+  //       this.className += " active-btn";
+  //     });
+  //   }
+
+  //Section active class
+  allSections.addEventListener("click", (e) => {
+    const id = e.target.dataset.id;
+    if (id) {
+      //   //remove selected from the other btns
+      //   sectBtns.forEach((btn) => {
+      //     btn.classList.remove("active");
+      //   });
+      //   e.target.classList.add("active");
+      const secBtnElement = document.querySelector('[data-id="' + id + '"]');
+      console.log(secBtnElement);
       let currentBtn = document.querySelectorAll(".active-btn");
       currentBtn[0].className = currentBtn[0].className.replace(
         "active-btn",
         ""
       );
-      this.className += " active-btn";
-    });
-  }
+      secBtnElement.className += " active-btn";
+
+      //hide other sections
+      sections.forEach((section) => {
+        section.classList.remove("active");
+      });
+
+      const element = document.getElementById(id);
+      element.classList.add("active");
+    }
+  });
 }
 
 PageTransitions();
